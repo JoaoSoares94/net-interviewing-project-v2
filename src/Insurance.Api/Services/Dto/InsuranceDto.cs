e94ac769f79226da6a90f1a744cec7fdfc09d1b6
João Soares <joao_soares_101@hotmail.com>
2023-03-26 22:32:11 +0100

    Creating OrdertDto and InsuranceDto classes

---
+++ src/Insurance.Api/Services/Dto/InsuranceDto.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Insurance.Api.Services.Dto
+{
+    public class InsuranceDto
+    {
+        public int ProductId { get; set; }
+        public float InsuranceValue { get; set; }
+        [JsonIgnore]
+        public string ProductTypeName { get; set; }
+        [JsonIgnore]
+        public bool ProductTypeHasInsurance { get; set; }
+        [JsonIgnore]
+        public float SalesPrice { get; set; }
+    }
+}
+
---
+++ src/Insurance.Api/Services/Dto/OrderDto.cs
@@ -0,0 +1,11 @@
+﻿
+using Insurance.Api.Services.Dto;
+using System.Collections.Generic;
+
+public class OrderDto
+{
+    public int OrderId { get; set; }
+    public List<InsuranceDto> Items { get; set; }
+
+    public int OrderInsurance { get; set; } = 0;
+}
