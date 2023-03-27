b90e0203aea967cd3198edc3082048fad9fedc5b
João Soares <joao_soares_101@hotmail.com>
2023-03-28 01:26:32 +0200

    Revamped HomeController , which will later be renamed to InsuranceController

--- src/Insurance.Api/Controllers/HomeController.cs
+++ src/Insurance.Api/Controllers/HomeController.cs
@@ -2,28 +2,83 @@ using Insurance.Api.Services.Dto;
 using Microsoft.AspNetCore.Mvc;
 using Insurance.Api.Services;
 using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
-public class HomeController : Controller
+[Route("api/insurance/insurance")]
+[ApiController]
+public class HomeController : ControllerBase
+{
+    private readonly IInsuranceService _insuranceService;
+    private static List<InsuranceDto> insuranceDtos = new List<InsuranceDto>()
     {
-    private readonly MySettings _settings;
-
-    public HomeController(MySettings settings)
-    {
-        _settings = settings;
-    }
-   
-
-    [HttpPost]
-        [Route("api/insurance/product")]
-        public InsuranceDto CalculateInsurance([FromBody] InsuranceDto toInsure)
+        new InsuranceDto()
         {
-            int productId = toInsure.ProductId;
-
-            BusinessRules.GetProductType(_settings.ProductApi, productId, ref toInsure);
-            BusinessRules.GetSalesPrice(_settings.ProductApi, productId, ref toInsure);
-            BusinessRules.CalculateInsurance(ref toInsure);
-
-            return toInsure;
+            ProductId = 572770,
+        },
+         new InsuranceDto()
+        {
+            ProductId = 715990,
+        },
+          new InsuranceDto()
+        {
+            ProductId = 827074,
+        },
+           new InsuranceDto()
+        {
+            ProductId = 828519,
+        },
+            new InsuranceDto()
+        {
+            ProductId = 837856,
         }
 
- }
+    };
+
+
+    public HomeController(IInsuranceService insuranceService)
+    {
+        _insuranceService = insuranceService;
+    }
+
+    [HttpGet]
+    [Route("product/{id}")]
+
+    public InsuranceDto GetById(int id)
+    {
+
+        return insuranceDtos.Find(x => x.ProductId == id);
+    }
+
+    [HttpGet]
+    [Route("products")]
+    public List<InsuranceDto> GetAll()
+    {
+        return insuranceDtos;
+    }
+
+
+    [HttpPost]
+    [Route("product")]
+    public ActionResult<InsuranceDto> CalculateInsurance([FromBody] InsuranceDto toInsure)
+    {
+        int productId = toInsure.ProductId;
+        var result = _insuranceService.CalculateInsurance(productId);
+        insuranceDtos.Add(result);
+
+        return result;
+    }
+
+
+    [HttpPost]
+    [Route("orders")]
+    public ActionResult<InsuranceDto> CalculateInsurance([FromBody] OrderDto toInsure)
+    {
+        toInsure.Items = insuranceDtos;
+        _insuranceService.CalculateOrder(toInsure);
+
+        return null;
+    }
+
+}
