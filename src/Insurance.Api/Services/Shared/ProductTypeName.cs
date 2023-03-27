9fa1b7be7e3be2dd7af2b904764f03400d0fb7c9
João Soares <joao_soares_101@hotmail.com>
2023-03-28 01:27:04 +0200

    Created a class to support product type verifications using constants

---
+++ src/Insurance.Api/Services/Shared/ProductTypeName.cs
@@ -0,0 +1,10 @@
+﻿
+static class ProductTypeName
+{
+    public const string Laptops = "Laptops";
+    public const string Smartphones = "Smartphones";
+    public const string DigitalCameras = "Digital cameras";
+
+}
+
+
