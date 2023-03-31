575ce7c2f2115aac503d984c3725cd7fed857dab
João Soares <joao_soares_101@hotmail.com>
2023-03-31 16:07:47 +0200

    implemented surcharge dto

---
+++ src/Insurance.Api/Services/Dto/SurchargeDto.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace Insurance.Api.Services.Dto
+{
+    public class SurchargeDto
+    {
+        [JsonRequired]
+        public int ProductTypeId { get; set; }
+        [JsonRequired]
+        public float SurchargeRate { get; set; }
+
+        public string ProductTypeName { get; set; }
+
+        public SurchargeDto()
+        {
+            
+        }
+    }
+    
+}
